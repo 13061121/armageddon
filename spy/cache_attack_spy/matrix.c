7dc97c6f2f2f041ccfd22a85d270a484a090f461
13061121 <13061121@buaa.edu.cn>
2017-05-16 22:11:53 +0800

    添加检测程序的初始版本

---
+++ spy/cache_attack_spy/matrix.c
@@ -0,0 +1,121 @@
+#include"matrix.h"
+
+bool
+initMatrix(Matrix** mtr){
+    if (*mtr == NULL){
+        *mtr = (Matrix *)malloc(sizeof(Matrix));
+    }
+    (*mtr)->ac = 0;
+    (*mtr)->cc = 0;
+    (*mtr)->addresses = NULL;
+    (*mtr)->thres = 0;
+    (*mtr)->thresholds = NULL;
+    (*mtr)->characters = NULL;
+    (*mtr)->matrixvalues = NULL;
+    
+    return true;
+}
+
+size_t
+getValue(uint64_t num, uint64_t thres, uint64_t threshold){
+    if (num<=thres)
+        return 0;
+    else if(num<=threshold)
+        return 1;
+    else
+        return 2;
+}
+
+bool
+isCharacter(size_t* values, int vc, size_t* valueline){
+    int i;
+    size_t t;
+    for(i=0;i<vc;++i){
+        t = values[i];
+        if ((t == 0 && valueline[i]!=0) || ((t&valueline[i]) == 0 ))
+            return false;
+    }
+    return true;
+}
+
+
+int
+getCharacters(Matrix* mtr, uint64_t* values,int* chars){
+    unsigned int i, j;
+    int charcount;
+    bool* characters;
+    size_t* vs = (size_t*)malloc(sizeof(size_t)*mtr->ac);
+
+    for(unsigned int i=0;i<mtr->ac;i++){
+        vs[i]=getValue(values[i],mtr->thres,mtr->thresholds[i]);
+        fprintf(stdout,"%d\t",vs[i]);
+    }
+    fprintf(stdout,"\n");
+    
+    characters = (bool*)malloc(sizeof(bool)*mtr->cc);
+    charcount = 0;
+
+    for(i=0;i<mtr->cc;++i){
+        if(isCharacter(vs,mtr->ac,mtr->matrixvalues[i])){
+            characters[i]=true;
+            ++charcount;
+        }
+        else
+            characters[i]=false;
+    }
+
+    chars = (int*)malloc(sizeof(int)*charcount);
+
+    for(i=j=0;i<mtr->cc;++i){
+        if(characters[i])
+            chars[j++]=i;
+    }
+
+    return charcount;
+}
+
+void
+printMatrix(FILE * const _Stream, Matrix *mtr){
+    if(mtr->ac !=0)
+        fprintf(_Stream, "address count:\n%d\n",mtr->ac);
+    if(mtr->cc != 0)
+        fprintf(_Stream, "character count:\n%d\n",mtr->cc);
+    if(mtr->thres!=0)
+        fprintf(_Stream, "thres:\n%d\n",mtr->thres);
+    if(mtr->addresses != NULL){
+        fprintf(_Stream, "addresses:\n");
+        for(unsigned int i=0;i<mtr->ac;++i){
+            fprintf(_Stream, "%8p\t",(void*)mtr->addresses[i]);
+        }
+        fprintf(_Stream, "\n");
+    }
+
+    if(mtr->thresholds != NULL){
+        fprintf(_Stream, "threshold:\n");
+        for(unsigned int i=0;i<mtr->ac;++i){
+            fprintf(_Stream, "%d\t",mtr->thresholds[i]);
+        }
+        fprintf(_Stream, "\n");
+    }
+
+    if(mtr->characters != NULL){
+        fprintf(_Stream, "chracters:\n");
+        for(unsigned int i=0;i<mtr->cc;++i){
+            fprintf(_Stream, "%s\t",mtr->characters[i]);
+        }
+        fprintf(_Stream, "\n");
+    }
+
+    if(mtr->matrixvalues != NULL){
+        fprintf(_Stream, "matrixvalues:\n");
+        for(unsigned int i=0;i<mtr->cc;++i){
+            for (unsigned int j = 0; j < mtr->ac; j++)
+            {
+                fprintf(_Stream, "%d\t",mtr->matrixvalues[i][j]);
+            }
+            fprintf(_Stream, "\n");
+        }
+        fprintf(_Stream, "\n");
+    }
+    
+}
